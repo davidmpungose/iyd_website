7c05ea1251dff5da38172d2b9f958d952fce86c5
unknown <mpungosedavid4@gmail.com>
2025-01-17 13:23:53 +0200

    Update blog url

--- services/urls.py
+++ services/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.BlogView.as_view(), name='blog-home'),
+    path('blog/', views.blog_view, name='blog-home'),
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article-detail'),
     path('article/<int:pk>/edit', views.UpdatePostView.as_view(), name='edit-article'),
     path('add-article/', views.create_post, name= 'add-article'),
