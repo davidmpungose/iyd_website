24441fd913461a1ba31be3beb7c82ee7511fbe03
unknown <mpungosedavid4@gmail.com>
2025-01-17 15:27:11 +0200

    Update Several Files for Database error check

--- boots/settings.py
+++ boots/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'boots.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {'default': {
-    'ENGINE': 'django.db.backends.mysql',
+    'ENGINE': 'django.db.backends.postgresql',
     **dj_database_url.config(conn_max_age=600, ssl_require=True)
     }               
 }
