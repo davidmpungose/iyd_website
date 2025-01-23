234ae26725095f74db97246d80b1cbfa8497a068
unknown <mpungosedavid4@gmail.com>
2025-01-23 22:12:48 +0200

    Creating Blog Under Construction Page

--- services/urls.py
+++ services/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.BlogView.as_view(), name='blog-home'),
-    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article-detail'),
-    path('article/<int:pk>/edit', views.UpdatePostView.as_view(), name='edit-article'),
-    path('add-article/', views.create_post, name= 'add-article'),
+    path('blog/', views.BlogView, name='blog-home'),
+    # path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article-detail'),
+    # path('article/<int:pk>/edit', views.UpdatePostView.as_view(), name='edit-article'),
+    # path('add-article/', views.create_post, name= 'add-article'),
     
 ]
--- services/views.py
+++ services/views.py
@@ -8,77 +8,81 @@ from django.contrib import messages
 from .models import BlogPost, BlogCategory
 from .forms import PostForm
 
-class BlogView(ListView):
-    model = BlogPost
-    template_name = 'blog/blog_plus.html'
-    paginate_by = 5
-
-    def get_queryset(self):
-        return super().get_queryset().order_by('-created_at')
-
-    def get_context_data(self, **kwargs):
-        context = super(BlogView, self).get_context_data(**kwargs)
-        categories = BlogCategory.objects.all()
-        context['categories'] = categories
-        return context
-
-class PostView(ListView):
-    model = BlogPost
-    template_name = 'blog/post.html'
-
-class ArticleDetailView(DetailView):
-    model = BlogPost
-    template_name = 'blog/article_details.html'
-
-    def get_context_data(self, **kwargs: Any): 
-        context = super().get_context_data(**kwargs)
-        context['post'] = self.object
-        return context
-
-class AddPostView(CreateView):
-    model = BlogPost
-    template_name = 'blog/add_article.html'
-    fields = '__all__'
+def BlogView(request):
+    return render(request, 'blog/uc.html')
 
 
-class UpdatePostView(UpdateView):
-    model = BlogPost
-    template_name = 'blog/edit_post.html'
-    fields = ['title', 'title_tag', 'image', 'content']
+# class BlogView(ListView):
+#     model = BlogPost
+#     template_name = 'blog/blog_plus.html'
+#     paginate_by = 5
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post'] = self.object  # Pass the current blog post object
-        return context
+#     def get_queryset(self):
+#         return super().get_queryset().order_by('-created_at')
 
-def some_view(request):
-    categories = BlogCategory.objects.all()
-    context = {'categories': categories}
-    return render(request, 'blog_plus.html', context)
+#     def get_context_data(self, **kwargs):
+#         context = super(BlogView, self).get_context_data(**kwargs)
+#         categories = BlogCategory.objects.all()
+#         context['categories'] = categories
+#         return context
 
-@login_required  # Ensures only logged-in users can create posts
-def create_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)  # Include uploaded files
-        if form.is_valid():
-            # Create a new BlogPost object
-            new_post = form.save(commit=False)  # Don't save yet
-            new_post.author = request.user  # Set the current user as author
+# class PostView(ListView):
+#     model = BlogPost
+#     template_name = 'blog/post.html'
 
-            # Check if a category is selected (optional)
-            if 'category' in request.POST:
-                selected_category = request.POST['category']
-                new_post.category = BlogCategory.objects.get(pk=selected_category)
+# class ArticleDetailView(DetailView):
+#     model = BlogPost
+#     template_name = 'blog/article_details.html'
 
-            new_post.save()  # Now save the post to the database
-            messages.info(request, 'New Blog Post Added Successfully.')
+#     def get_context_data(self, **kwargs: Any): 
+#         context = super().get_context_data(**kwargs)
+#         context['post'] = self.object
+#         return context
 
-            return redirect('blog-home')  # Redirect to blog homepage after success
-        else:
-            messages.warning(request, 'Something went wrong.')
-            return redirect('blog-home')
-    else:
-        form = PostForm()
+# class AddPostView(CreateView):
+#     model = BlogPost
+#     template_name = 'blog/add_article.html'
+#     fields = '__all__'
 
-    context = {'form': form}
-    return render(request, 'blog/add_article.html', context)
+
+# class UpdatePostView(UpdateView):
+#     model = BlogPost
+#     template_name = 'blog/edit_post.html'
+#     fields = ['title', 'title_tag', 'image', 'content']
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['post'] = self.object  # Pass the current blog post object
+#         return context
+
+# def some_view(request):
+#     categories = BlogCategory.objects.all()
+#     context = {'categories': categories}
+#     return render(request, 'blog_plus.html', context)
+
+# @login_required  # Ensures only logged-in users can create posts
+# def create_post(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)  # Include uploaded files
+#         if form.is_valid():
+#             # Create a new BlogPost object
+#             new_post = form.save(commit=False)  # Don't save yet
+#             new_post.author = request.user  # Set the current user as author
+
+#             # Check if a category is selected (optional)
+#             if 'category' in request.POST:
+#                 selected_category = request.POST['category']
+#                 new_post.category = BlogCategory.objects.get(pk=selected_category)
+
+#             new_post.save()  # Now save the post to the database
+#             messages.info(request, 'New Blog Post Added Successfully.')
+
+#             return redirect('blog-home')  # Redirect to blog homepage after success
+#         else:
+#             messages.warning(request, 'Something went wrong.')
+#             return redirect('blog-home')
+#     else:
+#         form = PostForm()
+
+#     context = {'form': form}
+#     return render(request, 'blog/add_article.html', context)
